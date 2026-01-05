2f7286b86c2ac51ac0443026a3f90449da0c46bf
Gneris Nextech <gnerisdev@gmail.com>
2026-01-05 16:11:02 -0300

    feat: base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    base: './', 
+
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
